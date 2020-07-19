dadea3ee60baeb098ebdcce88098ffdc02869454
Caio Jaime <kobashijaime@gmail.com>
2020-07-19 16:07:37 -0300

    Created table body.

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ let fieldes = document.querySelectorAll("#form-user-create [name]");
 let enviar = document.querySelector("#form-user-create");
 let user ={};
 
- enviar.addEventListener("submit", event=>{
+function addLine(dataUser){
 
-    event.preventDefault();
+  var tr = document.createElement("tr")
 
-      fieldes.forEach((fild, index) =>{
+  tr.innerHTML = `
+      <tr>
+          <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+          <td>${user.name}</td>
+          <td>${user.email}</td>
+          <td>${user.admin}</td>
+          <td>${user.birth}</td>
+          <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+          </td>
+      </tr> 
+  `;
+      document.querySelector("#table-users").appendChild(tr);
+}
 
-          if (fild.name == "gender"){
-            
-            if (fild.checked) {
-                user[fild.name] = fild.value;
-            }
-          } else {
-              user[fild.name] = fild.value;
+enviar.addEventListener("submit", event=>{
+
+  event.preventDefault();
+
+    fieldes.forEach((fild, index) =>{
+
+        if (fild.name == "gender"){
           
+          if (fild.checked) {
+              user[fild.name] = fild.value;
           }
-      });
-    console.log(user);
- });
+        } else {
+            user[fild.name] = fild.value;
+        
+        }
+    });
+
+      addLine(user);
+
+});
