cba803065b8d7bac8ab481cc6e6771ac5cca609d
Caio Jaime <kobashijaime@gmail.com>
2020-07-22 22:37:56 -0300

    Fix fileReader

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -53,9 +53,11 @@ class UserController {
                 reject(e);
             }
 
-            fileReader.readAsDataURL(file);
-    
-
+            if (file) {
+                fileReader.readAsDataURL(file);
+            }else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
 
        
